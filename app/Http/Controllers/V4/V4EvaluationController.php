6aad0f1dbad7a1e6dbc9dee2975d4fca368e4bde
ricky40kr <rhitik.kumar@teampumpkin.com>
2025-11-14 17:06:36 +0530

    fix: added missing mentorship type on eval submission

--- app/Http/Controllers/V4/V4EvaluationController.php
+++ app/Http/Controllers/V4/V4EvaluationController.php
@@ -2470,6 +2470,7 @@ class V4EvaluationController extends Controller
                                 'report_id' => null,
                                 'mentorship_weekday' => $validated['weekday'],
                                 'consultation_time' => $validated['time'],
+                                'mentorship_upload_type' => EvaluationSubmissionVersion::MENTORSHIP_UPLOAD_TYPE_SUBMITTED_VIDEO,
                                 'file_path' => $videoUrl,
                                 'uploaded_by' => $user->id,
                                 'file_meta' => $fileMeta,
@@ -3416,12 +3417,12 @@ class V4EvaluationController extends Controller
                         'Authorization' => 'Bearer ' . $token,
                         'Content-Type' => 'application/json',
                     ])->post($baseUrl . '/conversation/create', [
-                        'type' => 'single',
-                        'participants' => [
-                            $authUser->id,
-                            $submission->player_id
-                        ],
-                    ]);
+                                'type' => 'single',
+                                'participants' => [
+                                    $authUser->id,
+                                    $submission->player_id
+                                ],
+                            ]);
 
                     if ($response->successful() && isset($response->json()['_id'])) {
                         $conversationId = $response->json()['_id'];
@@ -4142,12 +4143,12 @@ class V4EvaluationController extends Controller
                             'Authorization' => 'Bearer ' . $token,
                             'Content-Type' => 'application/json',
                         ])->post($baseUrl . '/conversation/create', [
-                            'type' => 'single',
-                            'participants' => [
-                                $consultationRequest->submission->player_id,
-                                $user->id
-                            ],
-                        ]);
+                                    'type' => 'single',
+                                    'participants' => [
+                                        $consultationRequest->submission->player_id,
+                                        $user->id
+                                    ],
+                                ]);
 
                         if ($response->successful() && isset($response->json()['_id'])) {
                             $conversationId = $response->json()['_id'];
@@ -4423,12 +4424,12 @@ class V4EvaluationController extends Controller
                             'Authorization' => 'Bearer ' . $token,
                             'Content-Type' => 'application/json',
                         ])->post($baseUrl . '/conversation/create', [
-                            'type' => 'single',
-                            'participants' => [
-                                $mentorshipRequest->submission->player_id,
-                                $user->id
-                            ],
-                        ]);
+                                    'type' => 'single',
+                                    'participants' => [
+                                        $mentorshipRequest->submission->player_id,
+                                        $user->id
+                                    ],
+                                ]);
 
                         if ($response->successful() && isset($response->json()['_id'])) {
                             $conversationId = $response->json()['_id'];
