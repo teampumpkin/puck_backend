3fd5479322a3c5ec2987c39b6f25eed6849ddf58
Mihir Pipermitwala <mihir.pipermitwala@teampumpkin.com>
2025-11-29 19:18:18 +0530

    add role

--- routes/api.php
+++ routes/api.php
@@ -469,7 +469,7 @@ Route::prefix('v4')->group(function () {
             Route::get('/{userId}', [V4TeamController::class, 'getTeamsForProfileById']);
             Route::post('/{teamId}/members/{academyId?}', [V4TeamController::class, 'addRemoveTeamMembers']);
             Route::get('/{teamId}/details', [V4TeamController::class, 'getTeamDetails']);
-            Route::get('/{teamId}/members', [V4TeamController::class, 'getTeamMembers']);
+            Route::get('/{teamId}/members/{role?}', [V4TeamController::class, 'getTeamMembers']);
             Route::post('/create', [V4TeamController::class, 'createTeam']);
             Route::post('/{teamId}/update', [V4TeamController::class, 'updateTeam']);
             Route::delete('/{teamId}', [V4TeamController::class, 'deleteTeam']);
