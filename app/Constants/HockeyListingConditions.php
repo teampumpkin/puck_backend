8ba39e077da7dca8ec96ebf97e294fd8bb2a1f64
ricky40kr <rhitik.kumar@teampumpkin.com>
2026-05-20 17:22:29 +0530

    fix: snake casing of hockey listing condtions

--- app/Constants/HockeyListingConditions.php
+++ app/Constants/HockeyListingConditions.php
@@ -5,14 +5,14 @@ namespace App\Constants;
 class HockeyListingConditions
 {
     const NEW = 'new';
-    const USED_LIKE_NEW = 'Used - Like New';
-    const USED_GOOD = 'Used - Good';
-    const USED_FAIR = 'Used - Fair';
+    const USED_LIKE_NEW = 'used_like_new';
+    const USED_GOOD = 'used_good';
+    const USED_FAIR = 'used_fair';
 
     public static function all(): array
     {
         return [
-            self::NEW,
+            self::NEW ,
             self::USED_LIKE_NEW,
             self::USED_GOOD,
             self::USED_FAIR,
